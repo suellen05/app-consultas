9d78b56ddf02c2d84726fcecd9c3910da8ff98e3
Your Name <suellenwmj@hotmail.com>
2021-05-11 20:20:49 -0300

    Criação da pagina index e estruturade pastas padrão

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Consultas</title>
+</head>
+
+<body>
+    <main>
+        <header>
+            <h1>APP Consultas</h1>
+            <hr>
+        </header>
+        <nav>
+            <a href="#"><img src="img/medico.png">Cadastro de Médicos</a>>
+            <a href="#"><img src="img/paciente.png">Cadastro de pacientes</a>
+            <a href="#"><img src="img/calendario.png">Agendamento de Consultas</a>
+        </nav>
+        <div class="Conteudo">
+            <h2>Conteudo da aplicação</h2>
+        </div>
+        <footer>
+            &copy; Todos os direitos reservados
+        </footer>
+    </main>
+</body>
+
+</html>
