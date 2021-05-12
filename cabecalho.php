ed90584bff20c2b055539dac84c89c7c618f5d82
Your Name <suellenwmj@hotmail.com>
2021-05-11 21:49:46 -0300

    criar cabecalho e rodapé

---
+++ cabecalho.php
@@ -0,0 +1,7 @@
+<?php include "cabecalho.php"?>
+
+<div class="Conteudo">
+        <h2>Conteudo da aplicação</h2>
+     </div>
+
+     <?php include "rodape.php"?>
--- index.php
+++ index.php
@@ -6,26 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Consultas</title>
-</head>
+    <link rel="stylesheet" href="bootstrap-5/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css" type="text/css">
+    </head>
 
 <body>
-    <main>
-        <header>
+<main class="container">
+             <header>
             <h1>APP Consultas</h1>
             <hr>
         </header>
         <nav>
-            <a href="#"><img src="img/medico.png">Cadastro de Médicos</a>>
+            <a href="#"><img src="img/medico.png">Cadastro de Médicos</a>
             <a href="#"><img src="img/paciente.png">Cadastro de pacientes</a>
             <a href="#"><img src="img/calendario.png">Agendamento de Consultas</a>
         </nav>
-        <div class="Conteudo">
-            <h2>Conteudo da aplicação</h2>
-        </div>
+       
         <footer>
             &copy; Todos os direitos reservados
         </footer>
     </main>
+    <script src="bootstrp-5/bootstrap.bundle.min.js"></script>
+<script src="js/funcoes.js"></script>
+<script src="js/jquery.min.js"></script>
 </body>
 
 </html>
